167038270c4f735034990cbac3509db833dedc17
Shavkunov Pavel <shpavser@gmail.com>
2025-07-29 15:01:33 +0500

    White hosts

--- src/opaweb/main.go
+++ src/opaweb/main.go
@@ -72,7 +72,7 @@ func startAcme(e *common.Configuration, cs func(http.Handler) http.Handler) {
 
 	certManager := &autocert.Manager{
 		Prompt:     autocert.AcceptTOS,
-		HostPolicy: autocert.HostWhitelist(e.Acmehost),
+		HostPolicy: autocert.HostWhitelist(e.Acmehost...),
 		Cache:      autocert.DirCache(e.DirCache),
 	}
 
